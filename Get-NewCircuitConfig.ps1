f977c6af8763fedb43b6c162bb5f4f310a43a5b6
Austin de Coup-Crank <austindcc@gmail.com>
2020-08-20 12:45:21 -0700

    Create-IPAMSubnets

--- Get-NewCircuitConfig.ps1
+++ Get-NewCircuitConfig.ps1
@@ -1,7 +1,8 @@
+
 $BasePath = "C:\Users\adecoup\code\panos_config"
 . "$BasePath\config.ps1"
 $Networks = Import-Csv $BasePath\networks.csv
-$PANOSCmdPrefix = "set vsys vsys1"
+$CmdPrefix = "set vsys vsys1"
 
 
 Function Get-NetworkObject {
@@ -25,25 +26,24 @@ Function Get-PANOSAddressObjectName {
 }
 
 Function Get-PANOSAddressObjectConfig {
-
-    $PANOSConfig = @()
+    $Cfg = @()
     ForEach ($Net in $Networks) {
         If ($Net.network_object) {
             $AddressObjectName = Get-PANOSAddressObjectName -Net $Net
             $CIDR = "$($Net.network_id)/$($Net.prefix_length)"
             $Description = "$SiteName $($Net.Name)"
-            $CfgPrefix = "$PANOSCmdPrefix address $AddresObjectName"
-            $PANOSConfig += "$CfgPrefix ip-netmask $CIDR "
-            $PANOSConfig += "$CfgPrefix description `"$Description`""
-            $PANOSConfig += "$CfgPrefix tag `"$PANOSTag`""
+            $CfgPrefix = "$CmdPrefix address $AddresObjectName"
+            $Cfg += "$CfgPrefix ip-netmask $CIDR "
+            $Cfg += "$CfgPrefix description `"$Description`""
+            $Cfg += "$CfgPrefix tag `"$PANOSTag`""
         }
     }
-    Return $PANOSConfig
+    Return $Cfg
 }
 
 Function Get-PANOSAddressGroupConfig {
-    $PANOSConfig = @()
-    $CfgPrefix = "$PANOSCmdPrefix address-group"
+    $Cfg = @()
+    $CfgPrefix = "$CmdPrefix address-group"
     $GroupPrefix = $($SiteName -replace '\s+', '').toUpper()
     $AllNetworksGroupName = "$GroupPrefix-NETWORKS"
     ForEach ($Group in $PANOSNetworkGroupMap.GetEnumerator()) {
@@ -51,11 +51,30 @@ Function Get-PANOSAddressGroupConfig {
         ForEach ($NetName in $Group.Value) {
             $Net = Get-NetworkObject -Name $NetName
             $AddressObjectName = Get-PANOSAddressObjectName -Net $Net
-            $PANOSConfig += "$CfgPrefix $GroupName static $AddressObjectName"
-            $PANOSConfig += "$CfgPrefix $AllNetworksGroupName static $AddressObjectName"
+            $Cfg += "$CfgPrefix $GroupName static $AddressObjectName"
+            $Cfg += "$CfgPrefix $AllNetworksGroupName static $AddressObjectName"
         }
-        $PANOSConfig += "$CfgPrefix $GroupName tag `"$PANOSTag`""
+        $Cfg += "$CfgPrefix $GroupName tag `"$PANOSTag`""
     }
-    $PANOSConfig += "$CfgPrefix $AllNetworksGroupName tag `"$PANOSTag`""
-    Return $PANOSConfig
+    $Cfg += "$CfgPrefix $AllNetworksGroupName tag `"$PANOSTag`""
+    Return $Cfg
+}
+
+Function Create-IPAMSubnets {
+    # 2020-08-20:
+    #   Solarwinds API does not appear to allow creation of IPAM folders ('hierarchy groups')
+    #   The API is further limited in that it can only create subnet objects in "hierarchy groups",
+    #   not "groups" (subgroups within hierarchy groups). This means we cannot use the API to create subnets
+    #   using our existing group/subgroup schema.
+    # https://documentation.solarwinds.com/en/success_center/IPAM/Content/IPAM-SWIS-API-to-perform-IPAM-operations.htm
+    Import-Module SwisPowerShell
+    If (!$Swis -and !$OrionServer) {
+        $OrionServer = $Global:OrionServer = Read-Host 'Orion IP or FQDN'
+    }
+    If (!$Swis) {
+        $Swis = $Global:Swis = Connect-Swis -Hostname $OrionServer
+    }
+    ForEach ($Net in $Networks) {
+        Invoke-SwisVerb $swis IPAM.SubnetManagement CreateSubnetForGroup @($Net.network_id, $Net.prefix_length, "IP Networks")
+    }
 }
